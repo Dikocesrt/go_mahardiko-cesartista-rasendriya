70ad6894e760a75516a80d5a24223471d32556cd
Diko Cesartista <dikocesrt@gmail.com>
2024-02-24 10:04:15 +0700

    revised soal 1 time and space complexity

--- 04_Time-And-Space-Complexity/praktikum/soal-1.go
+++ 04_Time-And-Space-Complexity/praktikum/soal-1.go
@@ -1,23 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func primeNumber(number int) bool {
 	if number <= 1 {
 		return false
-	} else if number == 2 {
+	}
+	if number == 2 {
 		return true
-	} else {
-		if number % 2 == 0 {
+	}
+	if number%2 == 0 {
+		return false
+	}
+	maxDivisor := int(math.Sqrt(float64(number)))
+	for i := 3; i <= maxDivisor; i += 2 {
+		if number%i == 0 {
 			return false
-		} else {
-			if number % 3 == 0 {
-				return false
-			} else if number % 5 == 0 {
-				return false
-			} else if number % 7 == 0 {
-				return false
-			}
 		}
 	}
 	return true
@@ -25,7 +26,7 @@ func primeNumber(number int) bool {
 
 func main(){
 	fmt.Println(primeNumber(1000000007))
-	fmt.Println(primeNumber(13))
+	fmt.Println(primeNumber(121))
 	fmt.Println(primeNumber(17))
 	fmt.Println(primeNumber(20))
 	fmt.Println(primeNumber(35))
